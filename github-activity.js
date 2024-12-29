5d92fce3b94b2bb0807dd4e47d4ec89b0b607969
Ashish Prajapati <108178021+ashCode98@users.noreply.github.com>
2024-12-29 18:39:32 +0530

    Improve error handling in githubUserActivity function

--- github-activity.js
+++ github-activity.js
@@ -1,48 +1,43 @@
-#!/usr/bin/env node
 async function githubUserActivity(username) {
-    let userData;
-    const url = `https://api.github.com/users/${username}/events`;
-    const user = await fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then((data) => userData = data)
-        .catch(error => console.error('Error:', error));
+  const url = `https://api.github.com/users/${username}/events`;
+  const userData = await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error("Error:", error));
 
-    let key = userData[0].type;
-    let repo = userData[0].repo.name;
-    switch (key) {
-        case "PushEvent":
-            console.log(`pushed 1 commit to ${repo}`);
-            break;
+  if (!userData || userData.length == 0) {
+    console.error("No events found for the user");
+  }
 
-        case "IssueCommentEvent":
-            console.log(`opened a new issue in ${repo}`);
-            break;
+  let key = userData[0].type;
+  let repo = userData[0].repo.name;
+  switch (key) {
+    case "PushEvent":
+      console.log(`pushed 1 commit to ${repo}`);
+      break;
 
-        case "CreateEvent":
-            console.log(`Initialized a new repo ${repo}`);
-            break;
+    case "IssueCommentEvent":
+      console.log(`opened a new issue in ${repo}`);
+      break;
 
-        case "WatchEvent":
-            console.log(`Starred ${repo}`);
-            break;
+    case "CreateEvent":
+      console.log(`Initialized a new repo ${repo}`);
+      break;
 
-        case "ForkEvent":
-            console.log(`Forked ${repo}`);
-            break;
+    case "WatchEvent":
+      console.log(`Starred ${repo}`);
+      break;
 
-        default:
-            console.log("Enter the username correctly!!");
-            break;
-    }
+    case "ForkEvent":
+      console.log(`Forked ${repo}`);
+      break;
+
+    default:
+      console.log("Enter the username correctly!!");
+      break;
+  }
 }
 
 const args = process.argv.slice(1);
-args[0] = "github-activity";
 const username = args[1];
 
 !username ? console.error("404, user not found") : githubUserActivity(username);
